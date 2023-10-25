cc12ab191ebb2a63af904ce4c2e54bf0659ec26c
Marko Urbanc <urbancmarko1@gmail.com>
2023-10-25 03:15:14 +0200

    feat: :art: heatmap plot of absolute errors

--- Code/Suite/visualize.py
+++ Code/Suite/visualize.py
@@ -11,8 +11,6 @@ from constants import *
 
 # Load the results
 data = pd.read_hdf("Results/global_benchmarks.h5", key="Benchmarks")
-print(data.columns)
-
 # The aim is to plot a heatmap of the absolute error for each function
 # and each method.
 
@@ -20,12 +18,11 @@ error_matrix = np.zeros((len(to_benchmark), len(methods)))
 
 for i, func in enumerate(to_benchmark):
     name = func.__name__
-    print(f"Function: {name}")
+    # print(f"Function: {name}")
     for j, method in enumerate(methods):
-        print(f"\t\tMethod: {method}")
+        # print(f"\t\tMethod: {method}")
         cond = data.index.isin([name]) & (data["Method"] == method)
         err = data.loc[cond, "Absolute error"].values[0]
-        print(err)
         error_matrix[i, j] = np.mean(err, axis=0)
 
 # Plot the heatmap
@@ -64,7 +61,7 @@ ax.set_xlabel("Method")
 ax.set_ylabel("Function")
 
 # Rotate the tick labels and set their alignment.
-plt.setp(ax.get_xticklabels(), rotation=45, ha="right", rotation_mode="anchor")
+plt.setp(ax.get_xticklabels(), rotation=30, ha="right", rotation_mode="anchor")
 
 # Loop over data dimensions and create text annotations.
 for i in range(len(to_benchmark)):
@@ -82,6 +79,7 @@ for i in range(len(to_benchmark)):
 ax.set_title("Absolute error of the converged points")
 ax.set_aspect("equal")
 plt.axis("square")
+plt.subplots_adjust(left=0.18, bottom=0.2, right=0.98, top=0.93)
 plt.show()
 
 
