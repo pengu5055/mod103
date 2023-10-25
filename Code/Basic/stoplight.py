1107aea82a036bde41121f4643cab40d2a609faa
Marko Urbanc <urbancmarko1@gmail.com>
2023-10-25 22:29:47 +0200

    fix: :ambulance: apply some critical QOL patches

--- Code/Basic/stoplight.py
+++ Code/Basic/stoplight.py
@@ -6,20 +6,38 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy.optimize import minimize
 from src import *
+import palettable as pl
+import cmasher as cmr
 
-# --- Initialize ---
+# --- Default Parameters ---
 n = 100  # Number of time steps
 v_0 = 5  # Initial velocity
 l = 100 # Length till stoplight
 t_step = 1  # Time step
 KAPPA = 0.1  # Penalty parameter
 
-solution = stoplight_solver(n, v_0, l, t_step, KAPPA)
+# --- Parameter Sweeps ---
+KAPPA = np.linspace(0, 1, 10)
 
+output = []
+for i, k in enumerate(KAPPA):
+    print(f"Solving for parameter {i + 1} of {len(KAPPA)}")
+    solution = stoplight_solver(n, v_0, l, t_step, k)
+    output.append(solution)
 # --- Plot ---
+cm = pl.cartocolors.sequential.PurpOr_4.mpl_colormap
+colors = cmr.take_cmap_colors(cm, len(KAPPA), return_fmt="hex", cmap_range=(0.2, 0.8))
+
 fig, ax = plt.subplots()
 
 # Plot the function
-plt.plot(solution)
+for i in range(len(output)):
+    solution = output[i]
+    ax.plot(solution, label=f"\kappa={KAPPA[i]}", color=colors[i])
+
+ax.set_xlabel("Time step")
+ax.set_ylabel("Velocity")
+ax.set_title("Stoplight problem with different penalty parameters")
+plt.legend()
 plt.show()
 
